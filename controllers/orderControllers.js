faafb15e83d3360fe600a449dda631fb45188d42
tschlag <travis.schlageter@gmail.com>
2021-05-17 16:57:35 -0500

    added checkout function

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -11,3 +11,40 @@ module.exports.get_orders = async (req, res) => {
     const userId = req.params.id;
     Order.find({userId}).sort({date:-1}).then(orders => res.json(orders));
 }
+
+//func that checksout the user and let's them pay for their cart
+module.exports.checkout = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const {source} = req.body;
+        let cart = await Cart.findOne({userId});
+        let user = await User.findOne({_id: userId});
+        const email = user.email;
+        
+        if (cart) {
+            const charge = await stripe.charges.create({
+                amount: cart.bill,
+                currency: 'usd',
+                source: source,
+                receipt_email: email
+            });
+
+            if (!charge) throw Error('Payment Failed. Please Try Again.');
+            if (charge) {
+                const order = await Order.create({
+                    userId,
+                    items: cart.items,
+                    bill: cart.bill
+                });
+                const data = await Cart.findByIdAndDelete({_id: cart.id});
+                return res.status(201).send(order);
+            }
+        } else {
+            res.status(500).send('You do not have any items in your cart');
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send('Something went wrong.');
+    }
+}
