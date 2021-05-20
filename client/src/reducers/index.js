021b0c54fffd5ffbb6c692be03ff06f0c97be114
tschlag <travis.schlageter@gmail.com>
2021-05-20 12:09:50 -0500

    updating index redux file

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,2 +1,18 @@
 //index imports all of the other reducers
 //and then combines them using combineReducers function from Redux
+
+import {combineReducers} from 'redux';
+import itemReducer from './itemReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
+import cartReducer from './cartReducer';
+import orderReducer from './orderReducer';
+
+export default combineReducers({
+    item: itemReducer,
+    error: errorReducer,
+    auth: authReducer,
+    cart: cartReducer,
+    order: orderReducer
+});
+
